e75d694817cffd471f1911a1543aeda3491fc091
virashu <virashu@disroot.org>
2026-01-31 17:21:51 +0300

    refactor(bit): remove redundant code

--- crates/bit/src/lib.rs
+++ crates/bit/src/lib.rs
@@ -1,11 +1,9 @@
 mod bit;
 mod bits;
 mod error;
-mod functions;
 mod try_bit;
 
 pub use bit::Bit;
 pub use bits::Bits;
 pub use error::Error;
-pub use functions::{from_bits, get_bit};
 pub use try_bit::TryBit;
